065552d0c6ef4beea231f484428d385a45cfc5ce
Andrew Powell <andrew.powell@temple.edu>
2017-01-19 17:11:48 -0500

    Wanted to do one more test on the l1 cache

--- mips/testhdl_cache_0/main.s
+++ mips/testhdl_cache_0/main.s
@@ -3,163 +3,121 @@
 	.previous
 	.text
 	.align	2
-	.globl	main
-	.ent	main
-	.type	main, @function
-main:
-	.frame	$fp,56,$31		# vars= 32, regs= 2/0, args= 16, gp= 0
-	.mask	0xc0000000,-4
+	.ent	l1_cache_flush
+	.type	l1_cache_flush, @function
+l1_cache_flush:
+	.frame	$sp,0,$31		# vars= 0, regs= 0/0, args= 0, gp= 0
+	.mask	0x00000000,0
 	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
-	
-	addiu	$sp,$sp,-56
-	sw	$31,52($sp)
-	sw	$fp,48($sp)
-	move	$fp,$sp
-	sw	$0,40($fp)
-	j	$L2
-	nop
+	li	$2,-32			# 0xffffffffffffffe0
+	li	$3,268435456			# 0x10000000
+	and	$4,$4,$2
+	ori	$3,$3,0x4
+#APP
+	sw $4, 0($3)
+sw $0, 0($4)
 
-$L3:
-	lw	$3,40($fp)
-	lw	$2,40($fp)
-	nop
-	sll	$2,$2,1
-	move	$4,$2
-	lui	$2,%hi(data)
-	sll	$3,$3,2
-	addiu	$2,$2,%lo(data)
-	addu	$2,$3,$2
-	sw	$4,0($2)
-	lw	$2,40($fp)
-	nop
-	addiu	$2,$2,1
-	sw	$2,40($fp)
-$L2:
-	lw	$2,40($fp)
-	nop
-	slt	$2,$2,16
-	bne	$2,$0,$L3
-	nop
-
-	lui	$2,%hi(data)
-	addiu	$2,$2,%lo(data)
-	sw	$2,32($fp)
-	li	$2,64			# 0x40
-	sw	$2,36($fp)
-	lui	$2,%hi(l1_cache_flush)
-	addiu	$4,$2,%lo(l1_cache_flush)
-	lw	$5,32($fp)
-	lw	$6,36($fp)
-	jal	l1_cache_operate_on_line_range
-	nop
-
-	lui	$2,%hi(data)
-	addiu	$2,$2,%lo(data)
-	sw	$2,24($fp)
-	li	$2,64			# 0x40
-	sw	$2,28($fp)
-	lui	$2,%hi(l1_cache_invalidate)
-	addiu	$4,$2,%lo(l1_cache_invalidate)
-	lw	$5,24($fp)
-	lw	$6,28($fp)
-	jal	l1_cache_operate_on_line_range
-	nop
-
-	lui	$2,%hi(data)
-	addiu	$4,$2,%lo(data)
-	move	$5,$0
-	li	$6,64			# 0x40
-	jal	memset
-	nop
-
-	lui	$2,%hi(data)
-	addiu	$2,$2,%lo(data)
-	sw	$2,16($fp)
-	li	$2,64			# 0x40
-	sw	$2,20($fp)
-	lui	$2,%hi(l1_cache_flush)
-	addiu	$4,$2,%lo(l1_cache_flush)
-	lw	$5,16($fp)
-	lw	$6,20($fp)
-	jal	l1_cache_operate_on_line_range
-	nop
-
-	move	$2,$0
-	move	$sp,$fp
-	lw	$31,52($sp)
-	lw	$fp,48($sp)
-	addiu	$sp,$sp,56
+#NO_APP
 	j	$31
-	nop
-
-	.set	macro
-	.set	reorder
-	.end	main
+	.end	l1_cache_flush
 	.align	2
 	.ent	l1_cache_invalidate
 	.type	l1_cache_invalidate, @function
 l1_cache_invalidate:
-	.frame	$fp,16,$31		# vars= 8, regs= 1/0, args= 0, gp= 0
-	.mask	0x40000000,-8
+	.frame	$sp,0,$31		# vars= 0, regs= 0/0, args= 0, gp= 0
+	.mask	0x00000000,0
 	.fmask	0x00000000,0
-	addiu	$sp,$sp,-16
-	sw	$fp,8($sp)
-	move	$fp,$sp
-	sw	$4,16($fp)
-	sw	$0,0($fp)
-	lw	$2,16($fp)
-	#nop
-	sw	$2,4($fp)
-	lw	$3,0($fp)
-	li	$2,268435456			# 0x10000000
-	addu	$4,$3,$2
-	lw	$3,4($fp)
 	li	$2,-32			# 0xffffffffffffffe0
-	and	$2,$3,$2
+	and	$4,$4,$2
+	li	$3,268435456			# 0x10000000
 #APP
-	sw $2, 0($4)
-sw $0, 0($2)
+	sw $4, 0($3)
+sw $0, 0($4)
 
 #NO_APP
-	move	$sp,$fp
-	lw	$fp,8($sp)
-	addiu	$sp,$sp,16
 	j	$31
 	.end	l1_cache_invalidate
 	.align	2
-	.ent	l1_cache_flush
-	.type	l1_cache_flush, @function
-l1_cache_flush:
-	.frame	$fp,16,$31		# vars= 8, regs= 1/0, args= 0, gp= 0
-	.mask	0x40000000,-8
+	.globl	main
+	.ent	main
+	.type	main, @function
+main:
+	.frame	$sp,32,$31		# vars= 0, regs= 3/0, args= 16, gp= 0
+	.mask	0x80030000,-8
 	.fmask	0x00000000,0
-	addiu	$sp,$sp,-16
-	sw	$fp,8($sp)
-	move	$fp,$sp
-	sw	$4,16($fp)
-	li	$2,4			# 0x4
-	sw	$2,0($fp)
-	lw	$2,16($fp)
-	#nop
-	sw	$2,4($fp)
-	lw	$3,0($fp)
-	li	$2,268435456			# 0x10000000
-	addu	$4,$3,$2
-	lw	$3,4($fp)
-	li	$2,-32			# 0xffffffffffffffe0
-	and	$2,$3,$2
-#APP
-	sw $2, 0($4)
-sw $0, 0($2)
+	.set	noreorder
+	.set	nomacro
+	
+	addiu	$sp,$sp,-32
+	lui	$3,%hi(data)
+	sw	$16,16($sp)
+	li	$2,2			# 0x2
+	addiu	$16,$3,%lo(data)
+	sw	$31,24($sp)
+	sw	$17,20($sp)
+	sw	$0,%lo(data)($3)
+	sw	$2,4($16)
+	li	$3,4			# 0x4
+	li	$2,6			# 0x6
+	sw	$3,8($16)
+	sw	$2,12($16)
+	li	$3,8			# 0x8
+	li	$2,10			# 0xa
+	sw	$3,16($16)
+	sw	$2,20($16)
+	li	$3,12			# 0xc
+	li	$2,14			# 0xe
+	sw	$3,24($16)
+	sw	$2,28($16)
+	li	$3,16			# 0x10
+	li	$2,18			# 0x12
+	sw	$3,32($16)
+	sw	$2,36($16)
+	li	$3,20			# 0x14
+	li	$2,22			# 0x16
+	sw	$3,40($16)
+	lui	$17,%hi(l1_cache_flush)
+	sw	$2,44($16)
+	li	$3,24			# 0x18
+	li	$2,26			# 0x1a
+	sw	$3,48($16)
+	addiu	$4,$17,%lo(l1_cache_flush)
+	sw	$2,52($16)
+	li	$3,28			# 0x1c
+	li	$2,30			# 0x1e
+	move	$5,$16
+	li	$6,64			# 0x40
+	sw	$3,56($16)
+	sw	$2,60($16)
+	jal	l1_cache_operate_on_line_range
+	nop
 
-#NO_APP
-	move	$sp,$fp
-	lw	$fp,8($sp)
-	addiu	$sp,$sp,16
+	lui	$4,%hi(l1_cache_invalidate)
+	move	$5,$16
+	addiu	$4,$4,%lo(l1_cache_invalidate)
+	jal	l1_cache_operate_on_line_range
+	li	$6,64			# 0x40
+
+	move	$4,$16
+	move	$5,$0
+	jal	memset
+	li	$6,64			# 0x40
+
+	addiu	$4,$17,%lo(l1_cache_flush)
+	move	$5,$16
+	jal	l1_cache_operate_on_line_range
+	li	$6,64			# 0x40
+
+	lw	$31,24($sp)
+	lw	$17,20($sp)
+	lw	$16,16($sp)
+	move	$2,$0
 	j	$31
-	.end	l1_cache_flush
+	addiu	$sp,$sp,32
+
+	.set	macro
+	.set	reorder
+	.end	main
 
 	.comm	data,64,4
 	.ident	"GCC: (GNU) 4.1.2"
--- mips/testhdl_cache_0/plasmasoc.s
+++ mips/testhdl_cache_0/plasmasoc.s
@@ -3,146 +3,49 @@
 	.previous
 	.text
 	.align	2
-	.ent	l1_cache_find_range
-	.type	l1_cache_find_range, @function
-l1_cache_find_range:
-	.frame	$fp,32,$31		# vars= 24, regs= 1/0, args= 0, gp= 0
-	.mask	0x40000000,-8
+	.globl	l1_cache_operate_on_line_range
+	.ent	l1_cache_operate_on_line_range
+	.type	l1_cache_operate_on_line_range, @function
+l1_cache_operate_on_line_range:
+	.frame	$sp,32,$31		# vars= 0, regs= 4/0, args= 16, gp= 0
+	.mask	0x80070000,-4
 	.fmask	0x00000000,0
 	.set	noreorder
 	.set	nomacro
 	
 	addiu	$sp,$sp,-32
-	sw	$fp,24($sp)
-	move	$fp,$sp
-	sw	$4,32($fp)
-	sw	$5,36($fp)
-	sw	$6,40($fp)
-	sw	$7,44($fp)
-	li	$2,32			# 0x20
-	sw	$2,8($fp)
-	lw	$3,32($fp)
-	lw	$2,36($fp)
-	nop
-	addu	$2,$3,$2
-	sw	$2,4($fp)
-	lw	$3,4($fp)
-	li	$2,-32			# 0xffffffffffffffe0
-	and	$2,$3,$2
-	sw	$2,0($fp)
-	lw	$3,32($fp)
-	li	$2,-32			# 0xffffffffffffffe0
-	and	$3,$3,$2
-	lw	$2,40($fp)
-	nop
-	sw	$3,0($2)
-	lw	$3,0($fp)
-	lw	$2,8($fp)
-	nop
-	addu	$3,$3,$2
-	lw	$2,44($fp)
-	nop
-	sw	$3,0($2)
-	lw	$2,44($fp)
-	nop
-	lw	$2,0($2)
-	nop
-	sw	$2,20($fp)
-	lw	$3,4($fp)
-	lw	$2,0($fp)
-	nop
-	sltu	$2,$2,$3
-	beq	$2,$0,$L2
-	nop
+	sw	$18,24($sp)
+	sw	$31,28($sp)
+	sw	$17,20($sp)
+	sw	$16,16($sp)
+	beq	$6,$0,$L8
+	move	$18,$4
 
-	lw	$2,8($fp)
-	nop
-	sw	$2,16($fp)
-	j	$L4
-	nop
+	addu	$2,$6,$5
+	li	$4,-32			# 0xffffffffffffffe0
+	and	$3,$2,$4
+	sltu	$2,$3,$2
+	sll	$2,$2,5
+	addiu	$3,$3,32
+	and	$16,$5,$4
+	addu	$17,$2,$3
+	beq	$16,$17,$L8
+	move	$4,$16
 
-$L2:
-	sw	$0,16($fp)
-$L4:
-	lw	$4,20($fp)
-	lw	$2,16($fp)
-	nop
-	addu	$3,$4,$2
-	lw	$2,44($fp)
-	nop
-	sw	$3,0($2)
-	move	$sp,$fp
-	lw	$fp,24($sp)
+$L13:
+	jal	$18
+	addiu	$16,$16,32
+
+	bne	$17,$16,$L13
+	move	$4,$16
+
+$L8:
+	lw	$31,28($sp)
+	lw	$18,24($sp)
+	lw	$17,20($sp)
+	lw	$16,16($sp)
+	j	$31
 	addiu	$sp,$sp,32
-	j	$31
-	nop
-
-	.set	macro
-	.set	reorder
-	.end	l1_cache_find_range
-	.align	2
-	.globl	l1_cache_operate_on_line_range
-	.ent	l1_cache_operate_on_line_range
-	.type	l1_cache_operate_on_line_range, @function
-l1_cache_operate_on_line_range:
-	.frame	$fp,40,$31		# vars= 16, regs= 2/0, args= 16, gp= 0
-	.mask	0xc0000000,-4
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
-	
-	addiu	$sp,$sp,-40
-	sw	$31,36($sp)
-	sw	$fp,32($sp)
-	move	$fp,$sp
-	sw	$4,40($fp)
-	sw	$5,44($fp)
-	sw	$6,48($fp)
-	li	$2,32			# 0x20
-	sw	$2,16($fp)
-	lw	$2,48($fp)
-	nop
-	beq	$2,$0,$L11
-	nop
-
-	addiu	$2,$fp,20
-	addiu	$3,$fp,24
-	lw	$4,44($fp)
-	lw	$5,48($fp)
-	move	$6,$2
-	move	$7,$3
-	jal	l1_cache_find_range
-	nop
-
-	j	$L9
-	nop
-
-$L10:
-	lw	$2,20($fp)
-	lw	$3,40($fp)
-	move	$4,$2
-	jal	$3
-	nop
-
-	lw	$3,20($fp)
-	lw	$2,16($fp)
-	nop
-	addu	$2,$3,$2
-	sw	$2,20($fp)
-$L9:
-	lw	$3,20($fp)
-	lw	$2,24($fp)
-	nop
-	bne	$3,$2,$L10
-	nop
-
-$L11:
-	move	$sp,$fp
-	lw	$31,36($sp)
-	lw	$fp,32($sp)
-	addiu	$sp,$sp,40
-	j	$31
-	nop
 
 	.set	macro
 	.set	reorder
