16950d4eb3f8aa3aa115ce88e664715ba878383a
Andrew Powell <andrew.powell@temple.edu>
2017-01-28 16:43:30 -0500

    Created the axi lite read controller for the interrupt controller.

--- hdl/plasoc_int_axi4_write_cntrl.vhd
+++ hdl/plasoc_int_axi4_write_cntrl.vhd
@@ -20,7 +20,8 @@
 
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.ALL;                           
+use work.mlitesoc_pack.all;
 
 entity plasoc_int_axi4_write_cntrl is
     generic (
@@ -28,8 +29,7 @@ entity plasoc_int_axi4_write_cntrl is
         axi_address_width : integer := 16;
         axi_data_width : integer := 32;
         -- interrupt controller parameters.
-        int_interrupt_total : integer := 8;
-        int_cpu_int_enables_address : std_logic_vector := X"0000");
+        int_enables_address : std_logic_vector := X"0000");
     port (
         -- global interface.
         aclk : in std_logic;
@@ -46,13 +46,84 @@ entity plasoc_int_axi4_write_cntrl is
         axi_bvalid : out std_logic;
         axi_bready : in std_logic;
         axi_bresp : out std_logic_vector(1 downto 0);
-        -- cpu interface.
-        cpu_int_enables : out std_logic_vector(int_interrupt_total-1 downto 0));
+        -- interrupt controller interface.
+        int_enables : out std_logic_vector(axi_data_width-1 downto 0));
 end plasoc_int_axi4_write_cntrl;
 
 architecture Behavioral of plasoc_int_axi4_write_cntrl is
-
+    type state_type is (state_wait,state_write,state_response);
+    signal state : state_type := state_wait;
+    signal axi_awready_buff : std_logic;
+    signal axi_awaddr_buff : std_logic_vector(axi_address_width-1 downto 0);
+    signal axi_wready_buff : std_logic;
+    signal axi_bvalid_buff : std_logic;
 begin
-
+    axi_awready <= axi_awready_buff;
+    axi_wready <= axi_wready_buff;
+    axi_bvalid <= axi_bvalid_buff;
+    axi_bresp <= axi_resp_okay;
+    
+    -- Drive the axi read interface.
+    process (aclk)
+    begin
+        -- Perform operations on the clock's positive edge.
+        if rising_edge(aclk) then
+            if aresetn='0' then
+                axi_awready_buff <= '0';
+                axi_wready_buff <= '0';
+                axi_bvalid_buff <= '0';
+            else
+                -- Drive state machine.
+                case state is
+                -- WAIT mode.
+                when state_wait=>
+                    -- Sample address interface on handshake and go start
+                    -- performing the write operation.
+                    if axi_awvalid='1' and axi_awready_buff='1' then
+                        -- Prevent the master from sending any more control information.
+                        axi_awready_buff <= '0';
+                        -- Sample the address sent from the master.
+                        axi_awaddr_buff <= axi_awaddr;
+                        -- Begin to read data to write.
+                        axi_wready_buff <= '1';
+                        state <= state_write;
+                    -- Let the master interface know the slave is ready
+                    -- to receive address information.
+                    else
+                        axi_awready_buff <= '1';
+                    end if;
+                -- WRITE mode.
+                when state_write=>
+                    -- Wait for handshake.
+                    if axi_wvalid='1' and axi_wready_buff='1' then
+                        -- Prevent the master from sending any more data.
+                        axi_wready_buff <= '0';
+                        -- Only sample written data if the address is valid.
+                        if axi_awaddr_buff=int_enables_address then
+                            -- Only sample the specified bytes.
+                            for each_byte in 0 to axi_data_width/8-1 loop
+                                if axi_wstrb(each_byte)='1' then
+                                    int_enables(7+each_byte*8 downto each_byte*8) <=
+                                        axi_wdata(7+each_byte*8 downto each_byte*8);
+                                end if;
+                            end loop;
+                        end if;
+                        -- Begin to transmit the response.
+                        state <= state_response;
+                        axi_bvalid_buff <= '1';
+                    end if;
+                -- RESPONSE mode.
+                when state_response=>
+                    -- Wait for handshake.
+                    if axi_bvalid_buff='1' and axi_bready='1' then
+                        -- Starting waiting for more address information on 
+                        -- successful handshake.
+                        axi_bvalid_buff <= '0';
+                        state <= state_wait;
+                    end if;
+                end case;
+            end if;
+        end if;
+    end process;
 
 end Behavioral;
