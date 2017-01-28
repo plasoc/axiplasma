841590fd0b97b2f3a58557c57deadd2fcaaaf4d6
Andrew Powell <andrew.powell@temple.edu>
2017-01-28 16:49:24 -0500

    What am I doing using two computers.. Only pushed just so I switch off to my deskptop.

---
+++ hdl/plasoc_int_axi4_read_cntrl.vhd
@@ -0,0 +1,35 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01/28/2017 04:46:42 PM
+-- Design Name: 
+-- Module Name: plasoc_int_axi4_read_cntrl - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.mlitesoc_pack.all;
+
+entity plasoc_int_axi4_read_cntrl is
+--  Port ( );
+end plasoc_int_axi4_read_cntrl;
+
+architecture Behavioral of plasoc_int_axi4_read_cntrl is
+
+begin
+
+
+end Behavioral;
