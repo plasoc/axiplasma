daa34b609d8f59c207205d1b37a274b32baa342d
Andrew Powell <andrew.powell@temple.edu>
2017-02-02 13:07:47 -0500

    Wrote the documentation for the Interrupt Controller's AXI4-Lite Write Controller.

--- hdl/plasoc/plasoc_int_axi4_write_cntrl.vhd
+++ hdl/plasoc/plasoc_int_axi4_write_cntrl.vhd
@@ -1,53 +1,49 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 01/28/2017 03:13:16 PM
--- Design Name: 
--- Module Name: plasoc_int_axi4_write_cntrl - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
+-------------------------------------------------------
+--! @author Andrew Powell
+--! @date January 28, 2017
+--! @brief Contains the entity and architecture of the 
+--! Interrupt Controller's Slave AXI4-Lite Write 
+--! Controller.
+-------------------------------------------------------
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;                           
 use work.plasoc_pack.all;
 
+--! The Write Controller implements a Slave AXI4-Lite Write 
+--! interface in order to allow a Master interface to write to
+--! the registers of the core.
+--!
+--! Information specific to the AXI4-Lite
+--! protocol is excluded from this documentation since the information can
+--! be found in official ARM AMBA4 AXI documentation.
 entity plasoc_int_axi4_write_cntrl is
     generic (
-        -- axi parameters.
-        axi_address_width : integer := 16;
-        axi_data_width : integer := 32;
-        -- interrupt controller parameters.
-        int_enables_address : std_logic_vector := X"0000");
+        -- AXI4-Lite Write parameters.
+        axi_address_width : integer := 16;						--! Defines the AXI4-Lite Address Width.
+        axi_data_width : integer := 32;							--! Defines the AXI4-Lite Data Width.
+        -- Interrupt Controller parameters.
+        int_enables_address : std_logic_vector := X"0000"		--! Defines the offset for the Interrupt Enables register.
+	);
     port (
-        -- global interface.
-        aclk : in std_logic;
-        aresetn : in std_logic;
-        -- axi write interface.
-        axi_awaddr : in std_logic_vector(axi_address_width-1 downto 0);
-        axi_awprot : in std_logic_vector(2 downto 0);
-        axi_awvalid : in std_logic;
-        axi_awready : out std_logic;
-        axi_wvalid : in std_logic;
-        axi_wready : out std_logic;
-        axi_wdata : in std_logic_vector(axi_data_width-1 downto 0);
-        axi_wstrb : in std_logic_vector(axi_data_width/8-1 downto 0);
-        axi_bvalid : out std_logic;
-        axi_bready : in std_logic;
-        axi_bresp : out std_logic_vector(1 downto 0);
-        -- interrupt controller interface.
-        int_enables : out std_logic_vector(axi_data_width-1 downto 0) := (others=>'0'));
+        -- Global interface.
+        aclk : in std_logic;												--! Clock. Tested with 50 MHz.
+        aresetn : in std_logic;												--! Reset on low.
+        -- Slave AXI4-Lite Write interface.
+        axi_awaddr : in std_logic_vector(axi_address_width-1 downto 0);		--! AXI4-Lite Address Write signal.
+        axi_awprot : in std_logic_vector(2 downto 0);						--! AXI4-Lite Address Write signal.
+        axi_awvalid : in std_logic;											--! AXI4-Lite Address Write signal.
+        axi_awready : out std_logic;										--! AXI4-Lite Address Write signal.
+        axi_wvalid : in std_logic;											--! AXI4-Lite Write Data signal.
+        axi_wready : out std_logic;											--! AXI4-Lite Write Data signal.
+        axi_wdata : in std_logic_vector(axi_data_width-1 downto 0);			--! AXI4-Lite Write Data signal.
+        axi_wstrb : in std_logic_vector(axi_data_width/8-1 downto 0);		--! AXI4-Lite Write Data signal.
+        axi_bvalid : out std_logic;											--! AXI4-Lite Write Response signal.
+        axi_bready : in std_logic;											--! AXI4-Lite Write Response signal.
+        axi_bresp : out std_logic_vector(1 downto 0);						--! AXI4-Lite Write Response signal.
+        -- Interrupt Controller interface.
+        int_enables : out std_logic_vector(axi_data_width-1 downto 0) := (others=>'0') --! Interrupt Enables register.
+	);
 end plasoc_int_axi4_write_cntrl;
 
 architecture Behavioral of plasoc_int_axi4_write_cntrl is
