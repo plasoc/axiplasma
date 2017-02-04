816d76d08f56e587b84fa59173980914848c8fa7
Andrew Powell <andrew.powell@temple.edu>
2017-02-03 23:04:35 -0500

    Reverted back to previous commit without Zybo stuff.

---
+++ hdl/testbenches/timer_interrupt_tb/zybo_pack.vhd
@@ -0,0 +1,29 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/03/2017 10:37:22 AM
+-- Design Name: 
+-- Module Name: nexys4_pack - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+package testbench_pack is
+
+    constant board : string := "zybo";
+
+end package;
---
+++ mips/apps/cache_app/main.c
@@ -0,0 +1,41 @@
+#include "plasmasoc.h"
+#define DATA_SIZE	(16)	
+unsigned data[DATA_SIZE];
+unsigned* noncacheable = (unsigned*)L1_CACHE_NONCACHEABLE_BASE_ADDRESS;
+
+void memset(void* destination, unsigned char value, unsigned bytes)
+{
+	/* Very slow way of writing a memset function. */
+	unsigned char* cur = (unsigned char*)destination;
+	unsigned char* end = cur+bytes;
+	for (; cur!=end; cur++)
+		*cur = value;
+}
+
+int main()
+{
+	int each_word;
+
+	/* Store some arbitraty data directly into memory. This will force a read first from memory. */
+	for (each_word=0; each_word<DATA_SIZE; each_word++)
+		noncacheable[each_word] += each_word*2;
+
+	/* Store some arbitrary data into cache. */
+	for (each_word=0; each_word<DATA_SIZE; each_word++)
+		data[each_word] = each_word*2;
+
+	/* Flush cache. This should force the cache to write data back to main memory. */
+	l1_cache_flush_range((unsigned)data,sizeof(data));
+
+	/* Invalidate cache. This should force the cache to invalidate the corresponding lines. */
+	l1_cache_invalidate_range((unsigned)data,sizeof(data));
+
+	/* Clear array. This should force cache to read data from main memory. */
+	memset((void*)data,0,sizeof(data));
+
+	/* Flush cache. This should force the zeroed cache to go into memory. */
+	l1_cache_flush_range((unsigned)data,sizeof(data));
+	return 0;
+}
+
+
