20ce4e282f1d01bf87af8b7c9b1e345f10708895
Kaleb Barrett <ktbarrett.connectflour@gmail.com>
2017-02-03 22:41:38 -0500

    Revert "Finally finished documenting the last piece of source code, the driver for the Plasma-SoC's Timer Core." Change made to support Zybo broke timing constraints in newest Vivado This reverts commit 2ad50aeb095257ee1bd5954924e4b8f368a3a24a.

--- mips/plasoc/plasoc_timer.h
+++ mips/plasoc/plasoc_timer.h
@@ -1,9 +1,3 @@
-/**
- * @author Andrew Powell
- * @date January 31, 2017
- * @brief Contains hardware definitions and driver for the Plasma-SoC's Timer Core.
- */
-
 #ifndef PLASOC_TIMER_H_
 #define PLASOC_TIMER_H_
 
@@ -12,69 +6,32 @@ extern "C"
 {
 #endif
 
-	/* Timer Core definitions. See plasoc_pac.vhd for more information on these hardware definitions. */
-	#define PLASOC_TIMER_CONTROL_OFFSET             (0)
-	#define PLASOC_TIMER_START_BIT_LOC              (0)
-	#define PLASOC_TIMER_RELOAD_BIT_LOC             (1)
-	#define PLASOC_TIMER_ACK_BIT_LOC                (2)
-	#define PLASOC_TIMER_DONE_BIT_LOC               (3)
-	#define PLASOC_TIMER_TRIG_VALUE_OFFSET          (4)
-	#define PLASOC_TIMER_TICK_VALUE_OFFSET          (8)
+	#define PLASOC_TIMER_CONTROL_OFFSET		(0)
+	#define PLASOC_TIMER_START_BIT_LOC		(0)
+	#define PLASOC_TIMER_RELOAD_BIT_LOC		(1)
+	#define PLASOC_TIMER_ACK_BIT_LOC		(2)
+	#define PLASOC_TIMER_DONE_BIT_LOC		(3)
+	#define PLASOC_TIMER_TRIG_VALUE_OFFSET	(4)
+	#define PLASOC_TIMER_TICK_VALUE_OFFSET	(8)
 	
-	/* Represents the Timer Core. */
 	typedef struct 
 	{
-		unsigned base_address; /**< Base address to Timer Core's registers. */
+		unsigned base_address;
 	}
 	plasoc_timer;
 	
-	/**
-	 * @brief Configures the Timer Core object. 
-	 * @param obj Pointer to the object.
-	 * @param base_address Base address of the Timer Core's registers.
-	 */
 	static inline __attribute__ ((always_inline))
 	void plasoc_timer_setup(plasoc_timer* obj,unsigned base_address)
 	{
 		obj->base_address = base_address;
 	}
 	
-	/**
-	 * @brief Starts the Timer Core in normal operation.
-	 *
-	 * The normal operation imples that the Tick Value register will
-	 * increment to the value in the Trigger Value register, and then stop.
-	 * The Done bit in the Control register will then be set to high until
-	 * the Time Core is stopped. 
-	 *
-	 * It should be noted the Trigger Value register is buffered when the
-	 * Timer Core is in operation, and writing a new value to it during
-	 * operation will not cause any effect until the Timer Core is stopped. 
-	 *
-	 * @param obj Pointer to the object.
-	 */
 	static inline __attribute__ ((always_inline))
 	void plasoc_timer_start(plasoc_timer* obj)
 	{
 		*(volatile unsigned*)(obj->base_address+PLASOC_TIMER_CONTROL_OFFSET) = (1<<PLASOC_TIMER_START_BIT_LOC);
 	}
 	
-	/**
-	 * @brief Starts the Timer Core in reload operation.
-	 *
-	 * Similar to normal operation, the Tick Value register will increment until
-	 * it equals the value in the Trigger Value register and then set the Done
-	 * bit. However, in the reload operation, the Tick Value will reset back to zero
-	 * and continue to increment back to the Trigger Value. The Done bit can be set 
-	 * back to low by setting the Ack bit with this method.
-	 *
-	 * It should be noted the Trigger Value register is buffered when the
-	 * Timer Core is in operation, and writing a new value to it during
-	 * operation will not cause any effect until the Timer Core is stopped. 
-	 *
-	 * @param obj Pointer to the object.
-	 * @param ack_flag Nonzero value will set the Ack bit.
-	 */
 	static inline __attribute__ ((always_inline))
 	void plasoc_timer_reload_start(plasoc_timer* obj,unsigned ack_flag)
 	{
@@ -83,21 +40,12 @@ extern "C"
 			((ack_flag)?(1<<PLASOC_TIMER_ACK_BIT_LOC):0);
 	}
 	
-	/**
-	 * @brief Stops the Timer Core.
-	 * @param obj Pointer to the object.
-	 */
 	static inline __attribute__ ((always_inline))
 	void plasoc_timer_stop(plasoc_timer* obj)
 	{
 		*(volatile unsigned*)(obj->base_address+PLASOC_TIMER_CONTROL_OFFSET) = 0;
 	}
 	
-	/**
-	 * @brief Gets the done bit.
-	 * @param obj Pointer to the object.
-	 * @return Nonzero value implies the Done bit is set, otherwise it is low.
-	 */
 	static inline __attribute__ ((always_inline))
 	unsigned plasoc_timer_get_done(plasoc_timer* obj)
 	{
@@ -105,16 +53,6 @@ extern "C"
 			(1<<PLASOC_TIMER_DONE_BIT_LOC);
 	}
 	
-	/**
-	 * @brief Sets the Trigger Value.
-	 * 
-	 * It should be noted the Trigger Value register is buffered when the
-	 * Timer Core is in operation, and writing a new value to it during
-	 * operation will not cause any effect until the Timer Core is stopped. 
-	 *
-	 * @param obj Pointer to the object.
-	 * @param trig_value The value to write to the Trigger Value register.
-	 */
 	static inline __attribute__ ((always_inline))
 	void plasoc_timer_set_trig_value(plasoc_timer* obj,unsigned trig_value)
 	{
