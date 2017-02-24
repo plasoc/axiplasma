b164ad9bca9cf25f80a84333a4f7c276e38115b3
Andrew Powell <andrew.powell@temple.edu>
2017-02-24 02:20:43 -0500

    Wrote relatively more sophisticated testing application for FreeRTOS. Hopefully it just works when I run it in the morning.

--- mips/apps/freertos_app/main.asm
+++ mips/apps/freertos_app/main.asm
@@ -6,51 +6,6 @@
 	.module	nooddspreg
 	.text
 	.align	2
-	.globl	gpio_isr
-	.set	nomips16
-	.set	nomicromips
-	.ent	gpio_isr
-	.type	gpio_isr, @function
-gpio_isr:
-	.frame	$sp,0,$31		# vars= 0, regs= 0/0, args= 0, gp= 0
-	.mask	0x00000000,0
-	.fmask	0x00000000,0
-	lw	$2,%gp_rel(gpio_obj)($28)
-	#nop
-	lw	$2,4($2)
-	#nop
-	sw	$2,%gp_rel(input_value)($28)
-	lw	$2,%gp_rel(gpio_obj)($28)
-	li	$3,3			# 0x3
-	sw	$3,0($2)
-	jr	$31
-	.end	gpio_isr
-	.size	gpio_isr, .-gpio_isr
-	.align	2
-	.globl	taskmain
-	.set	nomips16
-	.set	nomicromips
-	.ent	taskmain
-	.type	taskmain, @function
-taskmain:
-	.frame	$sp,0,$31		# vars= 0, regs= 0/0, args= 0, gp= 0
-	.mask	0x00000000,0
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
-$L3:
-	lw	$3,%gp_rel(input_value)($28)
-	lw	$2,%gp_rel(gpio_obj)($28)
-	nop
-	sw	$3,8($2)
-	b	$L3
-	nop
-
-	.set	macro
-	.set	reorder
-	.end	taskmain
-	.size	taskmain, .-taskmain
-	.align	2
 	.globl	FreeRTOS_TickISR
 	.set	nomips16
 	.set	nomicromips
@@ -63,9 +18,14 @@ FreeRTOS_TickISR:
 	addiu	$sp,$sp,-24
 	sw	$31,20($sp)
 	jal	xTaskIncrementTick
-	beq	$2,$0,$L5
+	bne	$2,$0,$L2
+	lw	$2,%gp_rel(yieldfromisr_flag)($28)
+	#nop
+	beq	$2,$0,$L3
+$L2:
+	sw	$0,%gp_rel(yieldfromisr_flag)($28)
 	jal	vTaskSwitchContext
-$L5:
+$L3:
 	lw	$31,20($sp)
 	lw	$2,%gp_rel(timer_obj)($28)
 	li	$3,7			# 0x7
@@ -79,6 +39,210 @@ $L5:
 
 	.end	FreeRTOS_TickISR
 	.size	FreeRTOS_TickISR, .-FreeRTOS_TickISR
+	.align	2
+	.globl	gpio_isr
+	.set	nomips16
+	.set	nomicromips
+	.ent	gpio_isr
+	.type	gpio_isr, @function
+gpio_isr:
+	.frame	$sp,0,$31		# vars= 0, regs= 0/0, args= 0, gp= 0
+	.mask	0x00000000,0
+	.fmask	0x00000000,0
+	lw	$2,%gp_rel(gpio_obj)($28)
+	li	$3,3			# 0x3
+	lw	$4,%gp_rel(task_input_obj)($28)
+	sw	$3,0($2)
+	.set	noreorder
+	.set	nomacro
+	j	vTaskNotifyGiveFromISR
+	addiu	$5,$28,%gp_rel(yieldfromisr_flag)
+	.set	macro
+	.set	reorder
+
+	.end	gpio_isr
+	.size	gpio_isr, .-gpio_isr
+	.align	2
+	.globl	task_input_code
+	.set	nomips16
+	.set	nomicromips
+	.ent	task_input_code
+	.type	task_input_code, @function
+task_input_code:
+	.frame	$sp,32,$31		# vars= 8, regs= 1/0, args= 16, gp= 0
+	.mask	0x80000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-32
+	sw	$31,28($sp)
+	li	$5,-1			# 0xffffffffffffffff
+$L12:
+	jal	ulTaskNotifyTake
+	li	$4,1			# 0x1
+
+	lw	$2,%gp_rel(gpio_obj)($28)
+	lw	$4,%gp_rel(queue_input_obj)($28)
+	lw	$2,8($2)
+	move	$7,$0
+	li	$6,-1			# 0xffffffffffffffff
+	addiu	$5,$sp,16
+	jal	xQueueGenericSend
+	sw	$2,16($sp)
+
+	b	$L12
+	li	$5,-1			# 0xffffffffffffffff
+
+	.set	macro
+	.set	reorder
+	.end	task_input_code
+	.size	task_input_code, .-task_input_code
+	.align	2
+	.globl	task_time_code
+	.set	nomips16
+	.set	nomicromips
+	.ent	task_time_code
+	.type	task_time_code, @function
+task_time_code:
+	.frame	$sp,32,$31		# vars= 8, regs= 1/0, args= 16, gp= 0
+	.mask	0x80000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-32
+	sw	$31,28($sp)
+	jal	xTaskGetTickCount
+	nop
+
+	sw	$2,16($sp)
+	addiu	$4,$sp,16
+$L16:
+	jal	vTaskDelayUntil
+	li	$5,20			# 0x14
+
+	lw	$4,%gp_rel(sem_time_obj)($28)
+	move	$7,$0
+	move	$6,$0
+	jal	xQueueGenericSend
+	move	$5,$0
+
+	b	$L16
+	addiu	$4,$sp,16
+
+	.set	macro
+	.set	reorder
+	.end	task_time_code
+	.size	task_time_code, .-task_time_code
+	.align	2
+	.globl	task_main_code
+	.set	nomips16
+	.set	nomicromips
+	.ent	task_main_code
+	.type	task_main_code, @function
+task_main_code:
+	.frame	$sp,48,$31		# vars= 8, regs= 5/0, args= 16, gp= 0
+	.mask	0x800f0000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-48
+	sw	$17,32($sp)
+	lui	$17,%hi(ticks_ary)
+	sw	$19,40($sp)
+	sw	$18,36($sp)
+	sw	$16,28($sp)
+	sw	$31,44($sp)
+	move	$16,$0
+	move	$19,$0
+	li	$18,1			# 0x1
+	addiu	$17,$17,%lo(ticks_ary)
+$L27:
+	lw	$4,%gp_rel(queue_input_obj)($28)
+	move	$7,$0
+	move	$6,$0
+	jal	xQueueGenericReceive
+	addiu	$5,$sp,16
+
+	bne	$2,$18,$L18
+	move	$2,$0
+
+	lw	$19,16($sp)
+	nop
+	nor	$5,$0,$19
+	li	$4,16			# 0x10
+	sll	$3,$18,$2
+$L35:
+	and	$3,$3,$5
+	beq	$3,$0,$L19
+	sll	$3,$2,2
+
+	addu	$3,$3,$17
+	sw	$0,0($3)
+$L19:
+	addiu	$2,$2,1
+	bne	$2,$4,$L35
+	sll	$3,$18,$2
+
+$L18:
+	lw	$4,%gp_rel(sem_time_obj)($28)
+	move	$7,$0
+	move	$6,$0
+	jal	xQueueGenericReceive
+	move	$5,$0
+
+	bne	$2,$18,$L27
+	move	$4,$0
+
+	li	$6,24			# 0x18
+	li	$5,16			# 0x10
+	sll	$2,$18,$4
+$L36:
+	and	$3,$19,$2
+	beq	$3,$0,$L22
+	sll	$3,$4,2
+
+	addu	$3,$3,$17
+	lw	$7,0($3)
+	nop
+	bne	$7,$6,$L23
+	nop
+
+	sw	$0,0($3)
+	and	$3,$2,$16
+	beq	$3,$0,$L24
+	nop
+
+$L22:
+	nor	$2,$0,$2
+	b	$L25
+	and	$16,$16,$2
+
+$L24:
+	or	$16,$16,$2
+$L25:
+	addiu	$4,$4,1
+$L34:
+	bne	$4,$5,$L36
+	sll	$2,$18,$4
+
+	lw	$2,%gp_rel(gpio_obj)($28)
+	nop
+	sw	$16,8($2)
+	b	$L27
+	nop
+
+$L23:
+	lw	$2,0($3)
+	nop
+	addiu	$2,$2,1
+	sw	$2,0($3)
+	b	$L34
+	addiu	$4,$4,1
+
+	.set	macro
+	.set	reorder
+	.end	task_main_code
+	.size	task_main_code, .-task_main_code
 	.align	2
 	.globl	FreeRTOS_UserISR
 	.set	nomips16
@@ -102,8 +266,8 @@ FreeRTOS_UserISR:
 	move	$16,$3
 	addiu	$17,$17,%lo(int_obj+4)
 	sltu	$3,$2,8
-$L14:
-	bne	$3,$0,$L12
+$L41:
+	bne	$3,$0,$L39
 	nop
 
 	lw	$31,28($sp)
@@ -112,7 +276,7 @@ $L14:
 	jr	$31
 	addiu	$sp,$sp,32
 
-$L12:
+$L39:
 	sll	$2,$2,3
 	addu	$2,$17,$2
 	lw	$3,0($2)
@@ -123,7 +287,7 @@ $L12:
 	lw	$2,%lo(int_obj)($16)
 	nop
 	lw	$2,4($2)
-	b	$L14
+	b	$L41
 	sltu	$3,$2,8
 
 	.set	macro
@@ -176,17 +340,17 @@ vAssertCalled:
 	.fmask	0x00000000,0
 	.set	noreorder
 	.set	nomacro
-	bne	$5,$0,$L20
+	bne	$5,$0,$L47
 	nop
 
 	lw	$2,%gp_rel(gpio_obj)($28)
 	li	$3,65535			# 0xffff
 	sw	$3,8($2)
-$L19:
-	b	$L19
+$L46:
+	b	$L46
 	nop
 
-$L20:
+$L47:
 	jr	$31
 	nop
 
@@ -197,7 +361,13 @@ $L20:
 	.section	.rodata.str1.4,"aMS",@progbits,1
 	.align	2
 $LC0:
-	.ascii	"taskmain\000"
+	.ascii	"main\000"
+	.align	2
+$LC1:
+	.ascii	"input\000"
+	.align	2
+$LC2:
+	.ascii	"time\000"
 	.section	.text.startup,"ax",@progbits
 	.align	2
 	.globl	main
@@ -206,16 +376,17 @@ $LC0:
 	.ent	main
 	.type	main, @function
 main:
-	.frame	$sp,32,$31		# vars= 0, regs= 2/0, args= 24, gp= 0
-	.mask	0x80010000,-4
+	.frame	$sp,40,$31		# vars= 0, regs= 3/0, args= 24, gp= 0
+	.mask	0x80030000,-4
 	.fmask	0x00000000,0
-	addiu	$sp,$sp,-32
+	addiu	$sp,$sp,-40
 	move	$4,$0
-	sw	$31,28($sp)
+	sw	$31,36($sp)
+	sw	$17,32($sp)
 	.set	noreorder
 	.set	nomacro
 	jal	OS_AsmInterruptEnable
-	sw	$16,24($sp)
+	sw	$16,28($sp)
 	.set	macro
 	.set	reorder
 
@@ -226,11 +397,11 @@ main:
 	lui	$3,%hi(int_obj+4)
 	addiu	$3,$3,%lo(int_obj+4)
 	addiu	$4,$4,%lo(int_obj+68)
-$L22:
+$L49:
 	addiu	$3,$3,8
 	.set	noreorder
 	.set	nomacro
-	bne	$3,$4,$L22
+	bne	$3,$4,$L49
 	sw	$0,-8($3)
 	.set	macro
 	.set	reorder
@@ -241,27 +412,86 @@ $L22:
 	addiu	$2,$2,%lo(int_obj)
 	addiu	$3,$3,%lo(gpio_isr)
 	sw	$3,12($2)
-	li	$4,5000			# 0x1388
+	li	$4,50000			# 0xc350
 	li	$3,1151401984			# 0x44a10000
-	sw	$0,16($2)
 	sw	$3,%gp_rel(timer_obj)($28)
+	sw	$0,16($2)
 	sw	$4,4($3)
 	lui	$3,%hi(FreeRTOS_TickISR)
 	addiu	$3,$3,%lo(FreeRTOS_TickISR)
-	li	$16,3			# 0x3
-	lui	$5,%hi($LC0)
-	lui	$4,%hi(taskmain)
+	lui	$4,%hi(ticks_ary)
 	sw	$3,4($2)
+	li	$6,64			# 0x40
+	move	$5,$0
+	addiu	$4,$4,%lo(ticks_ary)
+	.set	noreorder
+	.set	nomacro
+	jal	memset
 	sw	$0,8($2)
-	move	$7,$0
+	.set	macro
+	.set	reorder
+
+	move	$6,$0
+	li	$5,4			# 0x4
+	.set	noreorder
+	.set	nomacro
+	jal	xQueueGenericCreate
+	li	$4,8			# 0x8
+	.set	macro
+	.set	reorder
+
+	move	$5,$0
+	li	$4,8			# 0x8
+	sw	$2,%gp_rel(queue_input_obj)($28)
+	.set	noreorder
+	.set	nomacro
+	jal	xQueueCreateCountingSemaphore
+	li	$16,3			# 0x3
+	.set	macro
+	.set	reorder
+
+	lui	$5,%hi($LC0)
+	lui	$4,%hi(task_main_code)
 	sw	$0,20($sp)
 	sw	$16,16($sp)
+	move	$7,$0
 	li	$6,256			# 0x100
 	addiu	$5,$5,%lo($LC0)
+	addiu	$4,$4,%lo(task_main_code)
+	li	$17,5			# 0x5
 	.set	noreorder
 	.set	nomacro
 	jal	xTaskCreate
-	addiu	$4,$4,%lo(taskmain)
+	sw	$2,%gp_rel(sem_time_obj)($28)
+	.set	macro
+	.set	reorder
+
+	addiu	$2,$28,%gp_rel(task_input_obj)
+	lui	$5,%hi($LC1)
+	lui	$4,%hi(task_input_code)
+	sw	$2,20($sp)
+	sw	$17,16($sp)
+	move	$7,$0
+	li	$6,256			# 0x100
+	addiu	$5,$5,%lo($LC1)
+	.set	noreorder
+	.set	nomacro
+	jal	xTaskCreate
+	addiu	$4,$4,%lo(task_input_code)
+	.set	macro
+	.set	reorder
+
+	lui	$5,%hi($LC2)
+	lui	$4,%hi(task_time_code)
+	sw	$0,20($sp)
+	sw	$17,16($sp)
+	move	$7,$0
+	li	$6,256			# 0x100
+	addiu	$5,$5,%lo($LC2)
+	.set	noreorder
+	.set	nomacro
+	jal	xTaskCreate
+	addiu	$4,$4,%lo(task_time_code)
 	.set	macro
 	.set	reorder
 
@@ -272,34 +502,34 @@ $L22:
 	li	$3,1			# 0x1
 	sw	$3,0($2)
 	jal	vTaskStartScheduler
-	lw	$31,28($sp)
-	lw	$16,24($sp)
+	lw	$31,36($sp)
+	lw	$17,32($sp)
+	lw	$16,28($sp)
 	move	$2,$0
 	.set	noreorder
 	.set	nomacro
 	jr	$31
-	addiu	$sp,$sp,32
+	addiu	$sp,$sp,40
 	.set	macro
 	.set	reorder
 
 	.end	main
 	.size	main, .-main
-	.globl	led_state
+	.globl	yieldfromisr_flag
 	.section	.sbss,"aw",@nobits
 	.align	2
-	.type	led_state, @object
-	.size	led_state, 4
-led_state:
+	.type	yieldfromisr_flag, @object
+	.size	yieldfromisr_flag, 4
+yieldfromisr_flag:
 	.space	4
-	.globl	update_flag
-	.section	.sdata,"aw",@progbits
-	.align	2
-	.type	update_flag, @object
-	.size	update_flag, 4
-update_flag:
-	.word	1
 
-	.comm	input_value,4,4
+	.comm	ticks_ary,64,4
+
+	.comm	task_input_obj,4,4
+
+	.comm	sem_time_obj,4,4
+
+	.comm	queue_input_obj,4,4
 
 	.comm	gpio_obj,4,4
 
--- mips/apps/freertos_app/main.c
+++ mips/apps/freertos_app/main.c
@@ -1,5 +1,8 @@
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
+#include "queue.h"
+#include "string.h"
 #include "plasoc_cpu.h"
 #include "plasoc_int.h"
 #include "plasoc_timer.h"
@@ -14,17 +17,30 @@
 #define INT_PLASOC_GPIO_ID			(1)
 #define INT_XILINX_CDMA_ID			(2)
 
+#define GPIO_AMOUNT				(16)
+#define TICK_THRESHOLD				(25)
+#define TIMER_THRESHOLD				(20)
+#define QUEUE_AMOUNT				(8)
+#define SEM_AMOUNT				(8)
+
 plasoc_int int_obj;
 plasoc_timer timer_obj;
 plasoc_gpio gpio_obj;
-volatile unsigned input_value;
-volatile unsigned update_flag = 1;
-volatile unsigned led_state = 0;
+QueueHandle_t queue_input_obj;
+SemaphoreHandle_t sem_time_obj;
+TaskHandle_t task_input_obj;
+volatile unsigned ticks_ary[GPIO_AMOUNT];
+volatile unsigned yieldfromisr_flag = 0;
 
+/* This ISR is necessary to ensure FreeRTOS runs in preemptive mode with
+  time slices. */
 void FreeRTOS_TickISR()
 {
-	if (xTaskIncrementTick()!=pdFALSE)
+	if ((xTaskIncrementTick()!=pdFALSE)||(yieldfromisr_flag))
+	{
+		yieldfromisr_flag = 0;
 		vTaskSwitchContext();
+	}
 	plasoc_timer_reload_start(&timer_obj,1);
 }
 
@@ -45,16 +61,80 @@ extern void vAssertCalled(const char* str, int val)
 
 void gpio_isr(void* ptr)
 {
-	input_value = plasoc_gpio_get_data_in(&gpio_obj);
 	plasoc_gpio_enable_int(&gpio_obj,1);
+	vTaskNotifyGiveFromISR(task_input_obj,&yieldfromisr_flag);
 }
 
-void taskmain()
+void task_input_code()
 {
-	/* Run application's main loop. */
-	while (1) 
+	unsigned input_value;
+	while (1)
 	{
-		plasoc_gpio_set_data_out(&gpio_obj,input_value);
+		ulTaskNotifyTake(pdTRUE,portMAX_DELAY);
+		input_value = plasoc_gpio_get_data_out(&gpio_obj);
+		xQueueSend(queue_input_obj,&input_value,portMAX_DELAY);
+	}
+}
+
+void task_time_code()
+{
+	TickType_t xLastWakeTime;
+	xLastWakeTime = xTaskGetTickCount();
+	while (1)
+	{
+		vTaskDelayUntil(&xLastWakeTime,TIMER_THRESHOLD);
+		xSemaphoreGive(sem_time_obj);
+	}
+}
+
+void task_main_code()
+{
+	unsigned cur_value=0;
+	unsigned led_value=0;
+	while (1)
+	{
+		unsigned new_value;
+		if (xQueueReceive(queue_input_obj,&new_value,0)==pdTRUE)
+		{
+			unsigned each_sw;
+			for (each_sw=0;each_sw<GPIO_AMOUNT;each_sw++)
+				if ((~new_value)&(1<<each_sw))
+					ticks_ary[each_sw] = 0;
+			cur_value = new_value;
+		}
+		if (xSemaphoreTake(sem_time_obj,0)==pdTRUE)
+		{
+			unsigned each_led;
+			for (each_led=0;each_led<GPIO_AMOUNT;each_led++)
+			{
+				unsigned led_mask = (1<<each_led);
+				if (cur_value&led_mask)
+				{
+					if (ticks_ary[each_led]==(TICK_THRESHOLD-1))
+					{
+						ticks_ary[each_led] = 0;
+						if (led_value&led_mask)
+						{
+							led_value &= ~led_mask;
+						}
+						else
+						{
+							led_value |= led_mask;
+						}
+						
+					}
+					else
+					{
+						ticks_ary[each_led]++;
+					}
+				}
+				else
+				{
+					led_value &= ~led_mask;
+				}
+			}
+			plasoc_gpio_set_data_out(&gpio_obj,led_value);
+		}
 	}
 }
 
@@ -77,10 +157,22 @@ int main()
 	plasoc_timer_set_trig_value(&timer_obj,PLASOC_TIMER_MILLISECOND_CYCLES);
 	plasoc_int_attach_isr(&int_obj,INT_PLASOC_TIMER_ID,FreeRTOS_TickISR,0);
 
-	/* Create the tasks. */
+	/* Clear timer values. */
+	memset(ticks_ary,0,sizeof(ticks_ary));
+
+	/* Create FreeRTOS objects tasks. */
 	{
-		BaseType_t xReturned = xTaskCreate(taskmain,"taskmain",configMINIMAL_STACK_SIZE,0,3,0);
-		configASSERT( xReturned==pdPASS );
+		BaseType_t xReturned;
+		queue_input_obj = xQueueCreate(QUEUE_AMOUNT,sizeof(unsigned));
+		configASSERT(queue_input_obj!=0);
+		sem_time_obj = xSemaphoreCreateCounting(SEM_AMOUNT,0);
+		configASSERT(sem_time_obj!=0);
+		xReturned = xTaskCreate(task_main_code,"main",configMINIMAL_STACK_SIZE,0,3,0);
+		configASSERT(xReturned==pdPASS);
+		xReturned = xTaskCreate(task_input_code,"input",configMINIMAL_STACK_SIZE,0,5,&task_input_obj);
+		configASSERT(xReturned==pdPASS);
+		xReturned = xTaskCreate(task_time_code,"time",configMINIMAL_STACK_SIZE,0,5,0);
+		configASSERT(xReturned==pdPASS);
 	}
 
 	/* Enable all interrupts in the interrupt controller and start the timer in reload mode. */
