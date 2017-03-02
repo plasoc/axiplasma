8b282c181d2d1a8af3808edc908039699ef0d4a3
Andrew Powell <andrew.powell@temple.edu>
2017-03-01 19:02:50 -0500

    Made the change. The reason for this change in the first place is because it seems as though many IP with slave interfaces wait for valid before setting ready.

--- hdl/plasoc/plasoc_crossbar_axi4_write_cntrl.vhd
+++ hdl/plasoc/plasoc_crossbar_axi4_write_cntrl.vhd
@@ -71,7 +71,7 @@ architecture Behavioral of plasoc_crossbar_axi4_write_cntrl is
     end;
     
     function get_slave_permissions (
-        address_slave_handshakes : in std_logic_vector(axi_slave_amount-1 downto 0);
+        slave_valid : in std_logic_vector(axi_slave_amount-1 downto 0);
         master_iden : in std_logic_vector(axi_slave_amount*axi_master_iden_width-1 downto 0);
         reduced_data_enables : in std_logic_vector(axi_master_amount-1 downto 0) ) return
         std_logic_vector is
@@ -81,7 +81,7 @@ architecture Behavioral of plasoc_crossbar_axi4_write_cntrl is
         for each_master in 0 to axi_master_amount-1 loop
             for each_slave in 0 to axi_slave_amount-1 loop
                 master_iden_buff := to_integer(unsigned(master_iden((1+each_slave)*axi_master_iden_width-1 downto each_slave*axi_master_iden_width)));
-                if each_master=master_iden_buff and address_slave_handshakes(each_slave)='1' and reduced_data_enables(master_iden_buff)='0' then
+                if each_master=master_iden_buff and slave_valid(each_slave)='1' and reduced_data_enables(master_iden_buff)='0' then
                     slave_permissions(each_slave) := '1';
                     exit;
                 end if;
@@ -146,7 +146,7 @@ architecture Behavioral of plasoc_crossbar_axi4_write_cntrl is
     end;
     
     function get_master_permissions (
-        response_master_handshakes : in std_logic_vector(axi_master_amount-1 downto 0);
+        master_valid : in std_logic_vector(axi_master_amount-1 downto 0);
         slave_iden : in std_logic_vector(axi_master_amount*axi_slave_iden_width-1 downto 0);
         reduced_response_enables : in std_logic_vector(axi_slave_amount-1 downto 0) ) return
         std_logic_vector is
@@ -156,7 +156,7 @@ architecture Behavioral of plasoc_crossbar_axi4_write_cntrl is
         for each_master in 0 to axi_master_amount-1 loop
             slave_iden_buff := to_integer(unsigned(slave_iden((1+each_master)*axi_slave_iden_width-1 downto each_master*axi_slave_iden_width)));
             for each_slave in 0 to axi_slave_amount-1 loop
-                if each_slave=slave_iden_buff and response_master_handshakes(each_master)='1' and reduced_response_enables(slave_iden_buff)='0' then
+                if each_slave=slave_iden_buff and master_valid(each_master)='1' and reduced_response_enables(slave_iden_buff)='0' then
                     master_permissions(each_master) := '1';
                     exit;
                 end if;
@@ -215,9 +215,9 @@ begin
     begin
         reduced_data_write_enables <= reduce_enables_master(axi_data_write_enables_buff);
     end process;
-    process (address_slave_handshakes,axi_write_master_iden,reduced_data_write_enables)
+    process (s_axi_awvalid,axi_write_master_iden,reduced_data_write_enables)
     begin
-        slave_permissions <= get_slave_permissions(address_slave_handshakes,axi_write_master_iden,reduced_data_write_enables);
+        slave_permissions <= get_slave_permissions(s_axi_awvalid,axi_write_master_iden,reduced_data_write_enables);
     end process;
     
     process (m_axi_bvalid,s_axi_bready,axi_write_slave_iden)
@@ -228,9 +228,9 @@ begin
     begin
         reduced_response_write_enables <= reduce_enables_slave(axi_response_write_enables_buff);
     end process;
-    process (response_master_handshakes,axi_write_slave_iden,reduced_response_write_enables)
+    process (m_axi_bvalid,axi_write_slave_iden,reduced_response_write_enables)
     begin
-        master_permissions <= get_master_permissions(response_master_handshakes,axi_write_slave_iden,reduced_response_write_enables);
+        master_permissions <= get_master_permissions(m_axi_bvalid,axi_write_slave_iden,reduced_response_write_enables);
     end process;
     
     process (aclk)
