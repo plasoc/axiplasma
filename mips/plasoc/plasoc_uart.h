515238692ac62e0af7ea43a9c4f005c0e646f834
Andrew Powell <andrew.powell@temple.edu>
2017-03-17 12:39:41 -0400

    Finished the documentation for the UART driver.

--- mips/plasoc/plasoc_uart.h
+++ mips/plasoc/plasoc_uart.h
@@ -16,8 +16,8 @@ extern "C"
 	#define PLASOC_UART_CONTROL_OFFSET		(0)
 	#define PLASOC_UART_STATUS_IN_AVAIL_BIT_LOC	(0)
 	#define PLASOC_UART_STATUS_OUT_AVAIL_BIT_LOC	(1)
-	#define PLASOC_UART_IN_OFFSET			(4)
-	#define PLASOC_UART_OUT_OFFSET			(8)
+	#define PLASOC_UART_IN_OFFSET			(4) /* In Fifo. */
+	#define PLASOC_UART_OUT_OFFSET			(8) /* Out Fifo. */
 
 	/**
 	 * @brief Represents the GPIO Core. 
@@ -40,7 +40,14 @@ extern "C"
 	}
 
 	/**
-	 * @brief Writes an 8-bit value to the UART
+	 * @brief Attempts to write an 8-bit value to the UART Core.
+	 * 
+	 * This method should be used in conjunction with plasoc_uart_get_status_out_avail.
+	 * If the FIFO associated with the Out Fifo register is full, then the byte 
+	 * written to the Out Fifo register will be lost.
+	 *
+	 * @param obj Pointer to the object.
+	 * @param byte The output data. Only the least significant byte is applicable.
 	 */
 	static inline __attribute__ ((always_inline))
 	void plasoc_uart_set_out(plasoc_uart* obj,unsigned byte)
@@ -48,18 +55,56 @@ extern "C"
 		*((volatile unsigned*)(obj->base_address+PLASOC_UART_OUT_OFFSET)) = byte;
 	}
 
+	/**
+	 * @brief Attempts to receive an 8-bit value from the UART Core.
+	 * 
+	 * This method should be used in conjunction with plasoc_uart_get_status_in_avail.
+	 * If the FIFO associated with the In Fifo register is empty, then the byte acquired
+	 * from the In Fifo register will be zero.
+	 *
+	 * Note that Status In Avail signal from the UART Core itself is also the
+	 * Status In Avail bit in the Control register. In order to reset this signal,
+	 * plasoc_uart_get_in must be called until the FIFO is empty.
+	 *
+	 * @param obj Pointer to the object.
+	 * @return The input data. Only the least significant byte is applicable.
+	 */
 	static inline __attribute__ ((always_inline))
 	unsigned plasoc_uart_get_in(plasoc_uart* obj)
 	{
 		return *((volatile unsigned*)(obj->base_address+PLASOC_UART_IN_OFFSET));
 	}
 
+	/**
+	 * @brief Checks to see whether or not input data is available.
+	 *
+	 * This method should be used in conjunction with plasoc_uart_get_in.
+	 * If the FIFO associated with the In Fifo register is empty, then the byte acquired
+	 * from the In Fifo register will be zero.
+	 *
+	 * Note that Status In Avail signal from the UART Core itself is also the
+	 * Status In Avail bit in the Control register. In order to reset this signal,
+	 * plasoc_uart_get_in must be called until the FIFO is empty.
+	 *
+	 * @param obj Pointer to the object.
+	 * @return A nonzero value implies the FIFO contains input data, otherwise the FIFO is empty.
+	 */
 	static inline __attribute__ ((always_inline))
 	unsigned plasoc_uart_get_status_in_avail(plasoc_uart* obj)
 	{
 		return (*((volatile unsigned*)(obj->base_address+PLASOC_UART_CONTROL_OFFSET)))&(1<<PLASOC_UART_STATUS_IN_AVAIL_BIT_LOC);
 	}
 
+	/**
+	 * @brief Checks to see whether or not output data can be written to the associated FIFO.
+	 *
+	 * This method should be used in conjunction with plasoc_uart_set_out.
+	 * If the FIFO associated with the Out Fifo register is full, then the byte 
+	 * written to the Out Fifo register will be lost.
+	 *
+	 * @param obj Pointer to the object.
+	 * @return A nonzero value implies there is available space in the FIFO for output data, otherwise the FIFO is full.
+	 */
 	static inline __attribute__ ((always_inline))
 	unsigned plasoc_uart_get_status_out_avail(plasoc_uart* obj)
 	{
